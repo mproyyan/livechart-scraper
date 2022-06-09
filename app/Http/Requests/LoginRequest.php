847da575efb37203ca644a07f7080413c51c5b01
royyan <mproyyan@gmail.com>
2022-06-09 17:15:16 +0700

    feat: implement rate limiter for login request

---
+++ app/Http/Requests/LoginRequest.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Str;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\TooManyRequestsHttpException;
+
+class LoginRequest extends FormRequest
+{
+    public const MAX_ATTEMPT = 5;
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+            'token_name' => ['string'],
+            'expired_at' => ['required', 'date_format:Y-m-d', 'after_or_equal:today'],
+        ];
+    }
+
+    /**
+     * authenticate request credentials
+     * 
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function authenticateOrFail()
+    {
+        $this->ensureIsNotRateLimited();
+
+        if (Auth::attempt($this->only(['email', 'password'])) === false) {
+            RateLimiter::hit($this->throttleKey());
+
+            throw ValidationException::withMessages([
+                'email' => trans('auth.failed')
+            ]);
+        }
+    }
+
+    /**
+     * Ensure the login request is not rate limited.
+     *
+     * @return void
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function ensureIsNotRateLimited()
+    {
+        if (RateLimiter::tooManyAttempts($this->throttleKey(), self::MAX_ATTEMPT) === false) {
+            return;
+        }
+
+        $seconds = RateLimiter::availableIn($this->throttleKey());
+
+        throw new TooManyRequestsHttpException($seconds);
+    }
+
+    /**
+     * Get the rate limiting throttle key for the request.
+     *
+     * @return string
+     */
+    public function throttleKey()
+    {
+        $email = $this->input('email');
+        return Str::lower("{$email}|{$this->ip()}");
+    }
+}
