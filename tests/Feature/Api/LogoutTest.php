da9d5cbd09ab72e327af53c0ebdc771f897f788f
royyan <mproyyan@gmail.com>
2022-06-10 18:53:43 +0700

    test(feature): test user logout

---
+++ tests/Feature/Api/LogoutTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Feature\Api;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Tests\Traits\ResponseStructure;
+use Tests\Traits\WithUser;
+
+class LogoutTest extends TestCase
+{
+    use RefreshDatabase, WithUser, ResponseStructure;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->setupUser();
+    }
+
+    public function test_user_can_logout()
+    {
+        $response = $this->withHeader('Authorization', 'Bearer ' . $this->token)
+            ->postJson('/api/logout');
+
+        $response->assertOk()
+            ->assertJsonStructure(['token' => [...$this->tokenStructure]])
+            ->assertJson([
+                'token' => [
+                    'status' => 'Revoked'
+                ]
+            ]);
+
+        $this->assertDatabaseMissing('personal_access_tokens', [
+            'tokenable_id' => $this->user->id
+        ]);
+    }
+
+    public function test_unauthorized_cannot_logout()
+    {
+        $response = $this->postJson('/api/logout');
+
+        $response->assertUnauthorized()
+            ->assertJsonStructure($this->httpApiExceptionStructure);
+    }
+}
