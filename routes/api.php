9e4996ea9a07eda6faac81f5071875abf9e38075
royyan <mproyyan@gmail.com>
2022-06-10 09:56:50 +0700

    feat: create endpoint for login request

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller(AuthController::class)->group(function () {
     Route::post('/register', 'register');
+    Route::post('/login', 'login');
 });
