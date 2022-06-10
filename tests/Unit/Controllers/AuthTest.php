2216dc58aecfe242edf2af8030f544ef4edea982
royyan <mproyyan@gmail.com>
2022-06-10 08:31:33 +0700

    test(unit): test login method

--- tests/Unit/Controllers/AuthTest.php
+++ tests/Unit/Controllers/AuthTest.php
@@ -7,12 +7,20 @@ use App\Http\Requests\RegisterUserRequest;
 use Mockery\MockInterface;
 use Tests\TestCase;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Requests\LoginRequest;
 use App\Models\User;
+use Illuminate\Support\Carbon;
+use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Http\JsonResponse;
+use Laravel\Sanctum\NewAccessToken;
+use Laravel\Sanctum\PersonalAccessToken;
+use Mockery;
 
 class AuthTest extends TestCase
 {
-    public function test_registration()
+    use WithFaker;
+
+    public function test_registration_method()
     {
         $this->mock(UserInterface::class, function (MockInterface $mock) {
             $mock->shouldReceive('create')->once()->with([])->andReturn(new User());
@@ -27,5 +35,35 @@ class AuthTest extends TestCase
         $user = $this->app->call([$auth, 'register']);
 
         $this->assertInstanceOf(JsonResponse::class, $user);
+    }
+
+    public function test_login_method()
+    {
+        $expiredAt = Carbon::createFromInterface($this->faker->dateTimeBetween('today', '+1 month'));
+
+        /** @var PersonalAccessToken $tokenMock */
+        $tokenMock = Mockery::mock(PersonalAccessToken::class, function (MockInterface $mock) {
+            $mock->shouldReceive('getAttribute', 'offsetExists');
+        });
+
+        $userMock = Mockery::mock(UserInterface::class, function (MockInterface $mock) use ($tokenMock, $expiredAt) {
+            $mock->shouldReceive('createExpirableToken')
+                ->once()
+                ->with('main', $expiredAt)
+                ->andReturn(new NewAccessToken($tokenMock, $this->faker->md5));
+        });
+
+        $this->mock(LoginRequest::class, function (MockInterface $mock) use ($expiredAt, $userMock) {
+            $mock->shouldReceive('authenticateOrFail')->once()->withNoArgs();
+            $mock->shouldReceive('user')->once()->withNoArgs()->andReturn($userMock);
+            $mock->shouldReceive('input')->once()->with('token_name')->andReturn('main');
+            $mock->shouldReceive('date')->once()->withSomeOfArgs('expired_at')->andReturn($expiredAt);
+        });
+
+        /** @var AuthController $auth */
+        $auth = $this->app->make(AuthController::class);
+        $token = $this->app->call([$auth, 'login']);
+
+        $this->assertInstanceOf(JsonResponse::class, $token);
     }
 }
