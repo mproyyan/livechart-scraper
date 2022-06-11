7fdeadc515dd1e837d1088f5eed21215347bcdbe
royyan <mproyyan@gmail.com>
2022-06-11 13:39:50 +0700

    feat: create base AnimeInterface

---
+++ app/Contracts/AnimeInterface.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Contracts;
+
+use Illuminate\Contracts\Pagination\Paginator;
+
+interface AnimeInterface extends BaseModelInterface
+{
+   /**
+    * Create new anime instance
+    */
+   public function __construct(
+      ?int $id = null,
+      ?string $title = null,
+      ?string $image = null,
+      ?string $synopsis = null,
+      ?array $formatted_synopsis = null,
+      ?array $genres = null,
+      ?string $type = null,
+      ?string $source = null,
+      ?int $episodes = null,
+      ?string $status = null,
+      ?array $duration = null,
+      ?array $aired = null,
+      ?string $season = null,
+      ?int $year = null,
+      ?array $studios = null
+   );
+
+   /**
+    * Get all animes in the current page.
+    *
+    * @return Paginator
+    */
+   public function all(int $page = 1): Paginator;
+
+   /**
+    * Get a anime by id.
+    *
+    * @param string $slug
+    *
+    * @return self
+    */
+   public function find(string $slug): self;
+}
