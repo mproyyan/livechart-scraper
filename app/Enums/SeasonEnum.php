7b7cd60072b3c2102ac22a84c563323c76a37340
royyan <mproyyan@gmail.com>
2022-06-11 15:21:36 +0700

    feat: add new static method for SeasonEnum

--- app/Enums/SeasonEnum.php
+++ app/Enums/SeasonEnum.php
@@ -9,6 +9,16 @@ enum SeasonEnum: string
    case Summer = 'Summer';
    case Fall = 'Fall';
 
+   public static function getSeasonByMonth($month)
+   {
+      return match ($month) {
+         'Jan', 'January', 'Feb', 'February', 'Mar', 'March' => self::Winter,
+         'Apr', 'April', 'May', 'Jun', 'June' => self::Spring,
+         'Jul', 'July', 'Aug', 'August', 'Sep', 'September' => self::Summer,
+         'Oct', 'October', 'Nov', 'November', 'Dec', 'December' => self::Fall
+      };
+   }
+
    public function season($month)
    {
       return match ($month) {
