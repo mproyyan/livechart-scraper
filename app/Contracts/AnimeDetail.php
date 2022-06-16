3b425800db4d4c0acbdcfa84bebcc4a04c37cfce
royyan <mproyyan@gmail.com>
2022-06-16 13:46:58 +0700

    feat: create AnimeDetailInterface

---
+++ app/Contracts/AnimeDetail.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Contracts;
+
+use Illuminate\Contracts\Routing\UrlRoutable;
+
+interface AnimeDetailInterface extends AnimeInterface, UrlRoutable
+{
+   public function find(string $slug);
+}
